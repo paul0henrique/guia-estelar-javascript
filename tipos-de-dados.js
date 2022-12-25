282e6230f4c3a070596ae9b995c53b09b0c451fd
Paulo <paulohenriquesms10@gmail.com>
2022-12-25 13:05:24 -0300

    add data types

---
+++ tipos-de-dados.js
@@ -0,0 +1,42 @@
+// STRINGS
+
+console.log("Paulo")
+
+console.log(`
+
+Paulo
+
+`)
+
+console.log(`${2+2}`)
+
+
+// NUMBERS
+
+console.log(5)
+
+// NaN = not a number
+// Infinity
+
+
+// BOOLEAN
+
+console.log(true)
+console.log(false)
+
+// UNDEFINED
+
+// NULL: definido, mas é um objeto que não possui nada
+
+// OBJECT: atributos e métodos
+
+console.log({
+    name: "Paulo",
+    age: 21
+})
+
+// ARRAY
+
+nomes = ["Paulo", "Henrique"]
+
+console.log(nomes[0])
