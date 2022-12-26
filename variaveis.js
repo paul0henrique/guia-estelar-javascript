7cd1b24386e5707542fbe6378f8bd5e437370190
Paulo <paulohenriquesms10@gmail.com>
2022-12-25 22:35:38 -0300

    add variáveis

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+
+// JS é uma linguagem fracamente tipada e dinâmica
+
+// var: global e local
+// devolve a variável como undefined caso ela exista, mas apareça depois (hoisting)
+var name = "Paulo"
+
+// let: local
+let idade = 3
+
+// const: local/imutável
+const valorMaximo = 3
+
+// Nomeação
+// camelCase
+// snakeCase
+
+let age = 2
+let _age = 3
+
+console.log('age == _age?', age == _age)
