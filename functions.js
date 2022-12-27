07af8463929d691b0dfb2e649a7852fae7eccf12
Paulo <paulohenriquesms10@gmail.com>
2022-12-27 00:10:47 -0300

    add parameters and arguments

--- functions.js
+++ functions.js
@@ -8,3 +8,11 @@ function createGrammarSubjects() {
 
 createGrammarSubjects()
 createGrammarSubjects()
+
+// parameters
+
+const sum = function(number1, number2){
+    console.log(number1 + number2)
+}
+
+sum(2,3) // arguments
