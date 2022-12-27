6f33463e9a9bb21bd4f7ddb6bc5c16a429c3f74a
Paulo <paulohenriquesms10@gmail.com>
2022-12-27 00:07:10 -0300

    add functions

---
+++ functions.js
@@ -0,0 +1,10 @@
+// function statment
+
+function createGrammarSubjects() {
+    console.log("Simple present")
+    console.log("Irregular verbs")
+    console.log("Adverbs of intensity")
+}
+
+createGrammarSubjects()
+createGrammarSubjects()
--- variaveis.js
+++ variaveis.js
@@ -1,12 +1,12 @@
 
-// JS é uma linguagem fracamente tipada e dinâmica
+// JS é uma linguage1m fracamente tipada e dinâmica
 
 // var: global e local
 // devolve a variável como undefined caso ela exista, mas apareça depois (hoisting)
 var name = "Paulo"
 
 // let: local
-let idade = 3
+let idade = 21
 
 // const: local/imutável
 const valorMaximo = 3
@@ -15,7 +15,22 @@ const valorMaximo = 3
 // camelCase
 // snakeCase
 
-let age = 2
-let _age = 3
+let age1 = 2
+let _age1 = 3
 
-console.log('age == _age?', age == _age)
+console.log('age1 == _age1?', age1 == _age1)
+
+console.log(typeof age1)
+
+// interpolação: template literals ou template strings
+
+console.log(`${name} tem ${idade} anos.`)
+
+// EXERCÍCIO
+
+let weight
+
+let name = "Paulo"
+let age = 21
+let stars = 4.9
+let isSubscribed = true
