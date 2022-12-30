dcce0eff5cfaba99aa7779f04e0575e5c45951d8
Paulo <paulohenriquesms10@gmail.com>
2022-12-30 00:09:50 -0300

    add ternário

---
+++ condicional.js
@@ -0,0 +1,12 @@
+// Operador condicional (ternário)
+
+// condicao, então valor1. se não, valor2.
+
+// condicao ? valor1 : valor2
+
+let teoriaDaComputacao = true
+let redesDeComputadores = true
+
+const niceCourse = teoriaDaComputacao && redesDeComputadores ? "Curso top" : "Curso não top"
+
+console.log(niceCourse)
