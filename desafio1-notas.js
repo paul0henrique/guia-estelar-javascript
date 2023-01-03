a7bd5e3da9d82b91a93965aa2ca62486b255360c
Paulo <paulohenriquesms10@gmail.com>
2023-01-03 11:55:42 -0300

    refact desafio 1

--- desafio1-notas.js
+++ desafio1-notas.js
@@ -1,22 +1,28 @@
 // Transformar notas do sistema numérico em notas ABC
 
-let nota = 100
+let nota = 80
 let notaA = nota >= 90
 let notaB = nota >= 80 && nota <= 89
 let notaC = nota >= 70 && nota <= 79
 let notaD = nota >= 60 && nota <= 69
 let notaF = nota < 60
+let notaConvertida
 
 if (notaA) {
-    console.log(`A nota ${nota} equivale à nota A`)
+    notaConvertida = "A"
+    console.log(`A nota ${nota} equivale à nota ${notaConvertida}`)
 } else if (notaB) {
-    console.log(`A nota ${nota} equivale à nota B`)
+    notaConvertida = "B"
+    console.log(`A nota ${nota} equivale à nota ${notaConvertida}`)
 } else if(notaC) {
-    console.log(`A nota ${nota} equivale à nota C`)
+    notaConvertida = "C"
+    console.log(`A nota ${nota} equivale à nota ${notaConvertida}`)
 } else if(notaD) {
-    console.log(`A nota ${nota} equivale à nota D`)
+    notaConvertida = "D"
+    console.log(`A nota ${nota} equivale à nota ${notaConvertida}`)
 }  else if(notaF) {
-    console.log(`A nota ${nota} equivale à nota F`)
+    notaConvertida = "F"
+    console.log(`A nota ${nota} equivale à nota ${notaConvertida}`)
 } else {
-    console.log(`A nota ${nota} é inválida`)
+    console.log(`A nota ${nota} é inválida.`)
 }
